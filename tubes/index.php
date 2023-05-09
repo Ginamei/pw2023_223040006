b9f18eefb84dd2ef5b9b6ac2fd1b5f87a451d0b8
unknown <ginameirina>
2023-05-09 11:04:06 +0700

    mengumpulkan desain UI tubes

---
+++ tubes/index.php
@@ -0,0 +1,287 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <!--Import materialize.css-->
+   <link type="text/css" rel="stylesheet" href="css/materialize.min.css" media="screen,projection" />
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+  <!-- My CSS -->
+  <link rel="stylesheet" href="css/style.css">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>TUBES 223040006</title>
+</head>
+<body>
+  <!-- navbar -->
+  <div class="navbar-fixed">
+    <nav class="teal darken-4">
+      <div class="container">
+        <div class="nav-wrapper">
+        <a href="#home"><img src="img/logosubank.png" class="brand-logo"></a>
+          <a href="#" data-target="mobile-nav" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+          <ul class="right hide-on-med-and-down">
+            <li><a href="#about">About</a></li>
+            <li><a href="#wisata">Wisata</a></li>
+            <li><a href="#tiket">Tiket</a></li>
+            <li><a href="#contact">Contact Us</a></li>
+            <li><a href="login.php">Login</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </div>
+  
+
+  <!-- sidenav -->
+  <ul class="sidenav" id="mobile-nav">
+    <li><a href="">About Us</a></li>
+    <li><a href="">WISATA</a></li>
+    <li><a href="">TIKET</a></li>
+    <li><a href="">Contact Us</a></li>
+    <li><a href="">Login</a></li>
+  </ul>
+
+  <!-- slider -->
+  <div class="slider">
+    <ul class="slides">
+      <li><a href="https://www.instagram.com/florawisata_dcastello/?igshid=YWJhMjlhZTc%3D"><img src="img//SUBANG JAWARA.png"> alt=""></a></li>
+      <li><a href="https://www.instagram.com/kebuntehciater.id/"><img src="img//S U B A N G J A W A B A R A T .png" alt=""></a></li>
+    </ul>
+  </div>
+
+   <!-- About Us -->
+   <section id="about" class="about scrollspy">
+    <div class="container">
+      <div class="row">
+      <div class="container">    
+          <form class="d-flex" role="search" style="margin-left: 75%;">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit">Search</button>
+          </form>
+        </div>
+        <h3 class="center light grey-text text-darken-3">About Us</h3>
+        <div class="col m4 s12">
+          <div class="center kotak">
+            <img src="img/curug cileat.jpg" width="270px" alt="" title="">
+            <p>CURUG CILEAT</p>
+            <a href="https://www.nativeindonesia.com/curug-cileat/">Read More</a>
+            <hr width="20%">
+          </div>
+        </div>
+        <div class="col m4 s12">
+          <div class="center kotak">
+            <img src="img/dcastello.jpg" width="250px"  alt="" title="">
+            <p>D'CASTELLO</p>
+            <a href="https://travel.kompas.com/read/2022/02/05/192858327/harga-tiket-jam-buka-dan-rute-ke-florawisata-dcastello-ciater?page=all">Read More</a>
+            <hr width="50%">
+          </div>
+        </div>
+        <div class="col m4 s12">
+          <div class="center kotak">
+            <img src="img/kebunteh.jpg" width="250px" alt="" title="">
+            <p>KEBUN TEH CIATER</p>
+            <a href="https://sikidang.com/kebun-teh-ciater/">Read More</a>
+            <hr width="50%">
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Wisata -->
+  <div id="wisata" class="parallax-container scrollspy">
+    <div class="parallax"><img src="img/kebun teh.jpg"></div>
+    <div class="container wisata">
+      <h3 class="center light white-text">SUBANG JAWA BARAT</h3>
+      <div class="row">
+        <div class="col m4 s12 center">
+          <img src="img/logosubank.png" alt="">
+        </div>
+        <div class="col m4 s12 center">
+          <img src="img/instagram1.png" alt="">
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- wisata -->
+  <section id="wisata" class="services grey lighten-3 scrollspy">
+    <div class="container">
+      <div class="row">
+        <h3 class="light center grey-text text-darken-3">Wisata Subang</h3>
+        <div class="col m4 s12">
+          <div class="card-panel center">
+            <h5>Curug Cileat</h5>
+            <p>Curug Cileat adalah sebuah curug atau air terjun yang terletak di kabupaten Subang bagian
+               Selatan, Provinsi Jawa Barat, Indonesia.Curug Cileat, destinasi wisata air terjun mempesona 
+               yang akan memanjakan siapa saja yang mengunjunginya ketika berlibur. Menikmati pesona indahnya 
+               alam yang mengagumkan pastinya akan mendamaikan jiwa dan pikiran yang penuh dengan kepenatan.
+               Indonesia memang menjadi salah satu negara dengan pesona wisata yang luar biasa.</p>
+          </div>
+        </div>
+        <div class="col m4 s12">
+          <div class="card-panel center">
+            <h5>D'Castello</h5>
+            <p>Taman Flora Wisata D'Castello Ciater berlokasi di Jl. Palasari Dua-Babakan Gn. No.16, Ciater,
+               Kecamatan Ciater, Kabupaten Subang, Jawa Barat. Letaknya berada tak jauh dari kawasan Tangkuban Perahu
+               Kawasan wisata D'Castello tidak hanya menyediakan spot foto menarik saja, Anda bisa mencoba berbagai
+               kegiatan lainnya. Mulai dari kulineran hingga bermain di wahana permainan yang pasti seru dan menyenangkan
+               Menikmati pemandangan yang indah serta berfoto di berbagai spot menarik di D'Castello Ciater</p>
+          </div>
+        </div>
+        <div class="col m4 s12">
+          <div class="card-panel center">
+            <h5>Kebun Teh Ciater</h5>
+            <p>Kebun Teh Ciater menjadi salah satu destinasi wisata alam di Subang yang bisa anda kunjungi 
+               bersama keluarga tercinta. Keindahan alam Subang yang mempesona, menyajikan destinasi wisata
+               rekomended untuk piknik bersama keluarga.Kabupaten Subang memang menjadi salah satu kota di 
+               Jawa Barat yang menyimpan banyak potensi wisata.Salah satu wisata alam mempesona
+               di Subang yaitu Kebun Teh Ciater, panorama hijau yang menyegarkan akan memanjakan sejauh mata
+               memandang.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- tiket -->
+  <section id="tiket" class="tiket scrollspy">
+    <div class="container">
+      <h3 class="light center grey-text text-darken-3">Tiket Masuk</h3>
+      <div class="row">
+        <div class="col m3 s12">
+          <div class="card">
+            <div class="card-image">
+              <img src="img/Curug .png">
+            </div>
+            <div class="card-content">
+              <p>WISATA ALAM CURUG CILEAT</p>
+              <p>15.000 Ribu/Orang</p>
+            </div>
+            <div class="card-action">
+              <a href="https://sikidang.com/curug-cileat/"
+                class="waves-effect teal darken-4 btn">Shop Now</a>
+            </div>
+          </div>
+        </div>
+        <div class="col m3 s12">
+          <div class="card">
+            <div class="card-image">
+              <img src="img/D' Castello Ciater.jpg">
+            </div>
+            <div class="card-content">
+              <p>WISATA D'CASTELLO</p>
+              <p>30.000 Ribu - 40.000 Ribu/Orang</p>
+            </div>
+            <div class="card-action">
+              <a href="https://www.instagram.com/florawisata_dcastello/?igshid=YWJhMjlhZTc%3D"
+                class="waves-effect teal darken-4 btn">Shop Now</a>
+            </div>
+          </div>
+        </div>
+        <div class="col m3 s12">
+          <div class="card">
+            <div class="card-image">
+              <img src="img/kebun.jpg">
+            </div>
+            <div class="card-content">
+              <p>WISATA ALAM KEBUN TEH CIATER</p>
+              <p>5.000/Orang</p>
+            </div>
+            <div class="card-action">
+              <a href="https://sikidang.com/kebun-teh-ciater/"
+                class="waves-effect teal darken-4 btn">Shop Now</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Contact Us -->
+  <section id="contact" class="contact grey lighten-3 scrollspy">
+    <div class="container">
+      <h3 class="light center grey-text text-darken-3">Contact Us</h3>
+      <div class="row">
+        <div class="col m5 s12">
+          <div class="card-panel teal darken-4 center white-text">
+            <i class="material-icons">email</i>
+            <h5>Contact</h5>
+            <p>Hallo, silahkan mengisi form yang disebelah kanan jika ada yang mau anda tanyakan atau anda beli.
+              Terimakasih.</p>
+          </div>
+          <ul class="collection with-header">
+            <li class="collection-header">
+              <h5>Our Office</h5>
+            </li>
+            <li class="collection-item">Wisata Subang</li>
+            <li class="collection-item"> Subang, Jawa Barat</li>
+            <li class="collection-item">West Java, Indonesia</li>
+          </ul>
+        </div>
+
+        <div class="col m7 s12">
+          <form>
+            <div class="card-panel">
+              <h5>please fill out this form</h5>
+              <div class="input-field">
+                <input type="text" name="name" id="name" required class="validate">
+                <label for="name">Name</label>
+              </div>
+              <div class="input-field">
+                <input type="email" name="email" id="email" class="validate">
+                <label for="email">Email</label>
+              </div>
+              <div class="input-field">
+                <input type="text" name="phone" id="phone">
+                <label for="phone">Phone Number</label>
+              </div>
+              <div class="input-field">
+                <textarea name="message" id="message" class="materialize-textarea"></textarea>
+                <label for="message">Message</label>
+              </div>
+              <button type="submit" class="btn teal darken-4">Send</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- footer -->
+  <footer class="teal darken-4 white-text center">
+    <p>WISATA SUBANG JAWA BARAT. Copyright 2023.</p>
+    <footer>
+
+
+
+
+      <!--JavaScript at end of body for optimized loading-->
+      <script type="text/javascript" src="js/materialize.min.js"></script>
+      <script>
+        const sideNav = document.querySelectorAll('.sidenav')
+        M.Sidenav.init(sideNav);
+
+        const slider = document.querySelectorAll('.slider');
+        M.Slider.init(slider, {
+          indicators: false,
+          height: 500,
+          transition: 600,
+          interval: 3000
+        });
+
+        const parallax = document.querySelectorAll('.parallax')
+        M.Parallax.init(parallax);
+
+        const materialbox = document.querySelectorAll('.materialboxed')
+        M.Materialbox.init(materialbox);
+
+        const scroll = document.querySelectorAll('.scrollspy')
+        M.ScrollSpy.init(scroll, {
+          scrollOffset: 50
+        });
+
+      </script>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+</body>
+</html>
